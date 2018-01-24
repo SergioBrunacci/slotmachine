ed010e5c784715eea401a2f2da7acf6b705efacc
SergioBrunacci <sergio.a.brunacci@gmail.com>
2018-01-23 21:23:37 -0500

    Connect view controller with main store board

--- slotmachine/ViewController.swift
+++ slotmachine/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+
+    @IBOutlet var labels: [UILabel]!
+    
+    @IBOutlet weak var scorelabel: UILabel!
+    
+    private var game = SlotMachineGame()
+    private var score = 0
+    
+    
+    @IBAction func reset(_ sender: UIButton) {
+        
+    }
+    
+    
+    @IBAction func spin(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
